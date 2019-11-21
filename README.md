aac148e42dfb0a919f8f877555dba2ef229dc263
hasagi <trichimtrich@gmail.com>
2019-11-21 19:09:10 +0800

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # PyCompressTest
-# PyCompressTest
--- com.py
+++ com.py
@@ -5,11 +5,18 @@
 - Compression ratio
 - Compression speed
 - Decompression speed
-- CProfile (?): CPU + Memory + Disk
+- memory-profiler / line-profiler / CProfile (?): CPU + Memory + Disk IO
 
 Note:
 - Check only for single file compression
 - Check only for frame, not stream mode, which maybe different a bit in production
+- Memory leakage might exists, separated library call is a good approach
+
+Manual:
+- pyqlz
+https://pypi.org/project/pyqlz/#files
+s/malloc.h/stdlib.h/
+
 """
 
 # https://github.com/google/brotli/tree/master/python
@@ -28,8 +35,14 @@ import lzo
 
 # - pithy
 
+# TODO: check which version
+# https://pypi.org/project/pyquicklz/#files
+# https://github.com/robottwo/quicklz
 # https://github.com/sergey-dryabzhinsky/python-quicklz
 import quicklz
+
+# https://pypi.org/project/pyqlz/
+import pyqlz
 
 # - snappy
 
@@ -45,12 +58,20 @@ import zstandard
 # https://pypi.org/project/zstd/
 import zstd
 
-import time
+
+import timeit
+
 
 def init_test():
     test_methods = {}
 
-    # brotli
+    """ brotli
+
+        brotli.compress(data, quality=...)
+            quality: 0 - 11
+        
+        brotli.decompress(data)
+    """
     for quality in range(0, 11 + 1):
         test_methods["brotli-{}".format(quality)] = {
             "compress": {
@@ -62,8 +83,19 @@ def init_test():
             },
         }
 
-    # lz4
-    for level in range(0, 16 + 1):
+
+    """ lz4
+
+        lz4.frame.compress(data, level=...)
+            level in:
+                lz4.frame.COMPRESSIONLEVEL_MIN: 0
+                lz4.frame.COMPRESSIONLEVEL_MAX: 16
+
+                lz4.frame.COMPRESSIONLEVEL_MINHC: 3
+        
+        lz4.frame.decompress(data)
+    """
+    for level in range(lz4.frame.COMPRESSIONLEVEL_MIN, lz4.frame.COMPRESSIONLEVEL_MAX + 1):
         test_methods["lz4-{}".format(level)] = {
             "compress": {
                 "func": lz4.frame.compress,
@@ -74,9 +106,20 @@ def init_test():
             },
         }
 
-    # lz4framed
-    for level in range(0, 16 + 1):
-        test_methods["lz4framed-{}".format(level)] = {
+
+    """ py-lz4framed
+
+        lz4framed.compress(data, level=...)
+            level in:
+                lz4framed.LZ4F_COMPRESSION_MIN: 0
+                lz4framed.LZ4F_COMPRESSION_MAX: 12
+
+                lz4framed.LZ4F_COMPRESSION_MIN_HC: 3
+
+        lz4framed.decompress(data)
+    """
+    for level in range(lz4framed.LZ4F_COMPRESSION_MIN, lz4framed.LZ4F_COMPRESSION_MAX + 1):
+        test_methods["py-lz4framed-{}".format(level)] = {
             "compress": {
                 "func": lz4framed.compress,
                 "kargs": {"level": level},
@@ -86,17 +129,68 @@ def init_test():
             },
         }
 
-    # zstandard
+
+    """ python-lzo
+    
+        lzo.compress(data, level)
+            level: 1 - 9
+
+        lzo.decompress(data)
+    """
+    for level in range(1, 9 + 1):
+        test_methods["python-lzo-{}".format(level)] = {
+            "compress": {
+                "func": lzo.compress,
+                "args": [level],
+            },
+            "decompress": {
+                "func": lzo.decompress,
+            },
+        }
+
+
+    """ pyqlz
+
+        pyqlz.compress(data)
+
+        pyqlz.decompress(data)
+    """
+    test_methods["pyqlz"] = {
+        "compress": {
+            "func": pyqlz.compress,
+        },
+        "decompress": {
+            "func": pyqlz.decompress,
+        },
+    }
+
+
+    """ zstandard
+
+        zstandard.compress(data)
+
+        zstandard.decompress(data)
+    """
     test_methods["zstandard"] = {
-        "compress": {"func": zstandard.compress},
+        "compress": {
+            "func": zstandard.compress,
+        },
         "decompress": {
             "func": zstandard.decompress,
         },
     }
 
-    # zstd
+
+    """ zstd
+
+        zstd.compress(data)
+
+        zstd.decompress(data)
+    """
     test_methods["zstd"] = {
-        "compress": {"func": zstd.compress},
+        "compress": {
+            "func": zstd.compress,
+        },
         "decompress": {
             "func": zstd.decompress,
         },
@@ -104,10 +198,11 @@ def init_test():
 
     return test_methods
 
+
 def measure_func(func, args, kargs):
-    t0 = time.time()
+    t0 = timeit.default_timer()
     ret = func(*args, **kargs)
-    t1 = time.time()
+    t1 = timeit.default_timer()
     return ret, t1 - t0
 
 
@@ -117,18 +212,12 @@ def run_single_test(method, data):
     compress_kargs = method["compress"].get("kargs", {}) # type: dict
     c_data, c_time = measure_func(compress_func, [data, *compress_args], compress_kargs)
 
-    ratio = len(c_data) / len(data) * 100
-    # c_time = c_time * 1000 # ms
-    # print(">>> Compression: time {0:.3f} ms -- ratio {1:.2f}".format(c_time, ratio))
-
-
     decompress_func = method["decompress"]["func"]
     decompress_args = method["decompress"].get("args", []) # type: list
     decompress_kargs = method["decompress"].get("kargs", {}) # type: dict
     dc_data, dc_time = measure_func(decompress_func, [c_data, *decompress_args], decompress_kargs)
 
-    # dc_time = dc_time * 1000 # ms
-    # print(">>> Decompression: time {0:.3f} ms".format(dc_time))
+    ratio = len(c_data) / len(data) * 100
 
     return ratio, c_time, dc_time
 
@@ -136,16 +225,20 @@ def run_single_test(method, data):
 
 def main():
     data1 = open("com_data1.json", "rb").read()
-    data2 = open("com_data2.exe", "rb").read()
-    data3 = open("com_data3.bin", "rb").read()
+    # data2 = open("com_data2.exe", "rb").read()
+    # data3 = open("com_data3.bin", "rb").read()
+    data3 = open("mft.bin", "rb").read()
 
     test_methods = init_test()
+    data = data1
+
+    data = b"a" * (10 * 1024 * 1024)
+
     for name, method in test_methods.items():
-        ratio, c_time, dc_time = run_single_test(method, data1)
+        ratio, c_time, dc_time = run_single_test(method, data)
         c_mbs = len(data) / c_time / 1024 / 1024
         dc_mbs = len(data) / dc_time / 1024 / 1024
-        print("[+] {0}: ratio {1:.2f}% , compress speed {2:.2f} MB/s , decompress speed {3:.2f} MB/s".format(name, ratio, c_mbs, dc_mbs))
-        break
+        print("[+] {0}: ratio {1:.3f}% , compress speed {2:.3f} MB/s , decompress speed {3:.3f} MB/s".format(name, ratio, c_mbs, dc_mbs))
 
 
 if __name__ == "__main__":
