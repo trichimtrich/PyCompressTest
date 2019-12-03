fddb5e88c77a03055bd95e3e2822abda6c27d674
hasagi <trichimtrich@gmail.com>
2019-12-03 10:59:13 +0800

    update install script

--- lib/install.sh
+++ lib/install.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+cat <<EOF
+0. alr installed them
+1. sudo apt install libsnappy-dev zlib1g-dev liblzo2-dev
+2. sudo yum install libsnappy-devel epel-release lzo-devel lzo-minilzo
+3. brew install snappy
+Choice [0-3] (default 0)?
+EOF
+
+read varname
+
+if [ $varname == "1" ]
+then
+    sudo apt install libsnappy-dev zlib1g-dev liblzo2-dev
+elif [ $varname == "2" ]
+then
+    sudo yum install libsnappy-devel epel-release lzo-devel lzo-minilzo
+elif [ $varname == "3" ]
+then
+    brew install snappy
+else
+    exit 1
+fi
+
+
+
 pip install -r requirements.txt
 git submodule update --init --recursive
 
