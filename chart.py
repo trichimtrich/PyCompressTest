3c650343f0950b601cbf9bab679269c0095d1d70
hasagi <trichimtrich@gmail.com>
2019-12-04 15:58:19 +0800

    finish generate chart

--- chart.py
+++ chart.py
@@ -14,6 +14,7 @@ def main():
 
     new_data = {}
     keys = []
+    max_mem = 0
     for codec, test_result in d.items():
         if re.match("^.+?-\d+", codec):
             # quality / level
@@ -33,6 +34,7 @@ def main():
         dc_speed = test_result["*"]["size"] / test_result["*"]["decompressed_time"]
 
         mem_usage = test_result["?"]["mem_usage"]
+        max_mem = max(max(mem_usage), max_mem)
 
         interval = test_result["?"]["interval"]
 
@@ -63,11 +65,8 @@ def main():
             end_test_idx = end_process_idx
             print("[!] end_test > end_process")
 
-
-        mem_usage_state = [0] * len(mem_usage)
-        # start_test -> end_test: value = 1
-        for i in range(start_test_idx, end_test_idx + 1):
-            mem_usage_state[i] = 1
+        comp_arr = []
+        decomp_arr = []
 
         for fn, record in test_result.items():
             if fn in ("*", "?"):
@@ -76,20 +75,24 @@ def main():
             mid_time = record["mid_time"]
             end_time = record["end_time"]
 
+            # start_test < start_time < mid_time < end_time < end_test
             start_time_idx = int((start_time - start_process) / interval)
-            mid_time_idx = int((mid_time - start_process) / interval)
+            if start_time_idx < start_test_idx:
+                start_time_idx = start_test_idx
+                print("[!] ... start_time < start_test")
+
             end_time_idx = int((end_time - start_process) / interval)
+            if end_time_idx > end_test_idx:
+                end_time_idx = end_test_idx
+                print("[!] ... end_time > end_test")
 
-            # TODO: warning / assert for these indexes
+            mid_time_idx = int((mid_time - start_process) / interval)
+            if not (start_time_idx <= mid_time_idx and mid_time_idx <= end_time_idx):
+                mid_time_idx = int((start_time_idx + end_time_idx) / 2)
+                print("[!] ... mid_time not in [start_time, end_time]")
 
-            # start_time -> mid_time : compressing . value 2
-            for i in range(start_time_idx, mid_time_idx + 1):
-                mem_usage_state[i] = 2
-
-            # mid_time -> end_time : decompressing . value 3
-            for i in range(mid_time_idx + 1, end_time_idx + 1):
-                mem_usage_state[i] = 3
-            
+            comp_arr.append((start_time_idx, mid_time_idx))
+            decomp_arr.append((mid_time_idx, end_time_idx))
 
         new_data[name].append({
             "codec": codec,
@@ -98,12 +101,19 @@ def main():
             "decompression_rate": dc_speed,
             "ratio": ratio,
             "mem_usage": mem_usage,
-            "mem_usage_state": mem_usage_state,
+            "mem_state": {
+                "compression": comp_arr,
+                "decompression": decomp_arr,
+                "test": (start_test_idx, end_test_idx),
+                "process": (start_process_idx, end_process_idx),
+            },
         })
 
-    x = open("x.html", "r").read()
+    max_mem = (max_mem / 100 + 1) * 100
+    x = open("chart.html", "r").read()
     x = x.replace("replace_this_data", json.dumps(new_data))
     x = x.replace("replace_this_keys", json.dumps(keys))
+    x = x.replace("replace_this_max", json.dumps(max_mem))
 
     open(sys.argv[2], "w").write(x)
 
